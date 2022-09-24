35522137f3eb5653161ec30ab718059cf5cd97d2
parvindr091719 <parinvdr091719@gmail.com>
2022-09-24 22:20:32 +0530

    done project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./styles.css";
 
-function App() {
+const App = () => {
+  const [url, setUrl] = useState([]);
+  const [num, setNum] = useState();
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUrl(data.data);
+      });
+  };
+
+  const inPut = (event) => {
+    const input_Data = +event.target.value;
+    setNum(input_Data);
+  };
+
+  const handleSubmit = () => {
+    const arr = url
+      .filter((obj) => obj.id === num)
+      .map((obj) => {
+        return (
+          <div key={obj.id} className="user-details">
+            <p>
+              <span>{obj.first_name}</span> <span>{obj.last_name}</span>
+            </p>
+            <img src={obj.avatar} alt="avatar" />
+            <p>{obj.email}</p>
+          </div>
+        );
+      });
+    setData(arr);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="list-users">
+        <h3 id="greet-users">Fetch API Data</h3>
+        <div>
+          <input type="text" value={num} onChange={inPut} />
+          <br /> <br />
+          <button onClick={handleSubmit}>Submit</button>
+        </div>
+        <div id="users">{data}</div>
+      </div>
+    </>
   );
-}
-
+};
 export default App;
+
